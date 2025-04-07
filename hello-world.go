fed8c2dea8e7092a06a2d249a21ca7865f623b2e
Gunjan Singh <gunjan.singh6@ibm.com>
2025-04-07 14:15:01 +0100

    hello world and values

---
+++ hello-world.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// sometimes the progrm can built into binaries using go build hello-world.go
+// after building one can also just execute the binary file ./hello-world
+// just to run use go run hello-world.go
